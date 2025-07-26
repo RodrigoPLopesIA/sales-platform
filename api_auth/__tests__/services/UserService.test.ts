73e26c574a7a97d3569c36bb66a907c03224d4c1
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-26 11:47:15 -0300

    test: import user repository in user service test

--- api_auth/__tests__/services/UserService.test.ts
+++ api_auth/__tests__/services/UserService.test.ts
@@ -1,22 +1,23 @@
-import { UserService } from "../../src/service/UserService"
-
+import { IUserRepository } from "../../src/interface/IUserRepository";
+import { UserService } from "../../src/service/UserService";
 
 describe("User Service Test", () => {
-    let userService: UserService
-    let userRepository: jest.Mock<IUserRepository>
-    beforeAll(() => {
-        userService = new UserService()
+  let userService: UserService;
+  let userRepository: jest.Mock<IUserRepository>;
+  beforeAll(() => {
+    userService = new UserService();
 
-        userRepository = jest.mock({
-            save: jest.fn()
-        })
-    })
+    userRepository = {
+      save: jest.fn(),
+    } as unknown as jest.Mock<IUserRepository>;
+  });
 
-    it("Should create a new user", () => {
+  it("Should create a new user", () => {
+    const result = userService.save({
+      email: "test@email.com",
+      password: "123456123456",
+    });
 
-
-        const result = userService.save({email: "test@email.com", password: "123456123456"})
-    
-        expect(result).toBeDefined()
-    })
-})
+    expect(result).toBeDefined();
+  });
+});
