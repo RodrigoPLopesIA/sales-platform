f44976d6fe83e2f392ae9559ed9540f4d408b360
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-26 11:50:58 -0300

    refactor: implemente dependency inversion of IUserRepository and change save method

--- api_auth/src/service/UserService.ts
+++ api_auth/src/service/UserService.ts
@@ -1,11 +1,14 @@
+import { IUserRepository } from "../interface/IUserRepository";
 import { User } from "../model/User";
 
 
 
 export class UserService{
-
-
-    public save(data: User) : String {
-        return "test"
+    private userRepository: IUserRepository
+    constructor(userRepository: IUserRepository) {
+        this.userRepository = userRepository;
+    }
+    public save(data: User) : User {
+        return this.userRepository.save(data)
     }
 }
