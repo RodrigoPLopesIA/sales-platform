79f27e2cb672367b030fa3094057d6305ae9754a
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-28 22:05:37 -0300

    test: create test to authenticate user and return error when credentials is invalid

--- api_auth/__tests__/services/UserService.test.ts
+++ api_auth/__tests__/services/UserService.test.ts
@@ -1,20 +1,16 @@
-import { compareSync } from "bcrypt";
 import { User } from "../../src/entity/User";
 import { IUserRepository } from "../../src/interface/IUserRepository";
-import { User as UserDTO } from "../../src/model/User";
-
+import { JWTService } from "../../src/service/JWTService";
+import bcrypt from 'bcrypt'
 import { UserService } from "../../src/service/UserService";
 jest.mock("../../src/data-source");
 
-jest.mock("bcrypt", () => ({
-  hashSync: jest.fn(() => "123456123456"),
-  compareSync: jest.fn(() => true)
-}));
 
 describe("User Service Test", () => {
   let userService: UserService;
   let userRepository: jest.Mocked<IUserRepository>;
   let mockUser: User;
+  let token: string
 
   beforeAll(() => {
     mockUser = {
@@ -26,17 +22,19 @@ describe("User Service Test", () => {
       createdAt: new Date("2024-01-01T10:00:00.000Z"),
       updatedAt: new Date("2024-01-01T10:00:00.000Z"),
     };
-
+    token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNjNzVhNGY4LTVlNjMtNGYxYS1iYjdiLWRjOGIyYmRmMGU4OSIsImVtYWlsIjoidGVzdEBlbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJSb2RyaWdvIiwibGFzdE5hbWUiOiJMb3BlcyIsImlhdCI6MTc1Mzc0NzQ3NywiZXhwIjoxNzUzNzUxMDc3fQ.SDwp6BVmxQ_3KncIWSizWKGofb4RD4ZsK8Pjff6syPE"
     userRepository = {
       save: jest.fn().mockResolvedValue(mockUser),
       existsByEmail: jest.fn().mockResolvedValue(false),
-      findByEmail: jest.fn().mockResolvedValue(mockUser)
+      findByEmail: jest.fn().mockResolvedValue(mockUser),
     } as jest.Mocked<IUserRepository>;
 
     userService = new UserService(userRepository);
   });
 
   it("Should create a new user", async () => {
+    bcrypt.hashSync = jest.fn().mockReturnValue("123456123456")
+
     const result: User = await userService.save({
       email: "test@email.com",
       password: "123456123456",
@@ -57,21 +55,41 @@ describe("User Service Test", () => {
         password: "123456123456",
       })
     ).rejects.toThrow("User with test@email.com already exists");
-
-
   });
-
 
   it("Should authenticate user", async () => {
 
-
+    JWTService.sign = jest.fn().mockReturnValue(token)
+    bcrypt.compareSync = jest.fn().mockReturnValue(true)
 
     const result = await userService.authenticate({
-        email: "test@email.com",
-        password: "123456123456",
-      })
-    
-    expect(result).toEqual(mockUser)
+      email: "test@email.com",
+      password: "123456123456",
+    });
 
-  })
+    expect(result.token).toEqual(token);
+    
+  });
+
+  it("Should throw credentials invalid user email", async () => {
+
+    userRepository.findByEmail.mockResolvedValue(null)
+    await expect(userService.authenticate({
+      email: "test@email.com",
+      password: "123456123456",
+    })).rejects.toThrow("Credentials invalid!");
+
+  });
+
+  it("Should throw credentials invalid user password invalid", async () => {
+
+    userRepository.findByEmail.mockResolvedValue(mockUser)
+    bcrypt.compareSync = jest.fn().mockReturnValue(false)
+
+    await expect(userService.authenticate({
+      email: "test@email.com",
+      password: "123456123456",
+    })).rejects.toThrow("Credentials invalid!");
+
+  });
 });
--- api_auth/src/service/UserService.ts
+++ api_auth/src/service/UserService.ts
@@ -2,34 +2,39 @@ import { IUserRepository } from "../interface/IUserRepository";
 import { User as UserDTO } from "../model/User";
 import { User } from "../entity/User";
 import HTTPException from "../exceptions/HTTPException";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import { Credentials } from "../model/Credentials";
+import { JWTService } from "./JWTService";
+import { Token } from "../model/Token";
 export class UserService {
-
   private userRepository: IUserRepository;
 
   constructor(userRepository: IUserRepository) {
     this.userRepository = userRepository;
   }
 
-  public async authenticate({email, password}: Credentials) : Promise<User>{
+  public async authenticate({ email, password }: Credentials): Promise<Token> {
     const user = await this.userRepository.findByEmail(email);
 
-    if(!user) throw new HTTPException(401, "Credentials invalid!")
+    if (!user) throw new HTTPException(401, "Credentials invalid!");
 
-    if(!bcrypt.compareSync(password, user.password)) throw new HTTPException(401, "Credentials invalid!")
-      
-    return user;
+    if (!bcrypt.compareSync(password, user.password))
+      throw new HTTPException(401, "Credentials invalid!");
+
+    const token = JWTService.sign({
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    });
+    return new Token(token);
   }
-    
-  public async save(data: UserDTO): Promise<User> {
-      
-    data.password = bcrypt.hashSync(data.password, 10)
-      
-      if(await this.userRepository.existsByEmail(data.email))
-        throw new HTTPException(400, `User with ${data.email} already exists`)
-      return await this.userRepository.save(data);
-      
 
+  public async save(data: UserDTO): Promise<User> {
+    data.password = bcrypt.hashSync(data.password, 10);
+
+    if (await this.userRepository.existsByEmail(data.email))
+      throw new HTTPException(400, `User with ${data.email} already exists`);
+    return await this.userRepository.save(data);
   }
 }
