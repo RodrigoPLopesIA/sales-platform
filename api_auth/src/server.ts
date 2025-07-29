3c2c4e164abc7a04090aea406befe8cf5104f970
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-28 22:53:33 -0300

    feat: import eslint and prettier to project

--- api_auth/src/server.ts
+++ api_auth/src/server.ts
@@ -1,28 +1,32 @@
-import "reflect-metadata";
-import express from "express";
-import userRouter from "./routes/UserRouter";
-import logger from "./logs/Logger";
-import HTTPErrorMiddleware from "./middlewares/HTTPErrorMiddleware";
-import { errors } from "celebrate";
+import 'reflect-metadata';
+import express from 'express';
+import userRouter from './routes/UserRouter';
+import logger from './logs/Logger';
+import HTTPErrorMiddleware from './middlewares/HTTPErrorMiddleware';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
-app.use("/api/v1/", userRouter);
+app.use('/api/v1/', userRouter);
 
-app.get("/api/v1/health", (req, res) => {
+app.get('/api/v1/health', (req, res) => {
   return res.status(200).json({
     status: 200,
-    message: "API auth health",
+    message: 'API auth health',
   });
 });
 
 app.use(HTTPErrorMiddleware.execute);
 
 app.listen(PORT, () =>
-  logger.info(`Server running at http://localhost:${PORT}/api/v1/health`)
+  logger.info(`Server running at http://localhost:${PORT}/api/v1/health`),
 );
 export default app;
